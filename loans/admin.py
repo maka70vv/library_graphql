054ae6922fdf8619400779096217080d0a9851fc
maka70vv <25.makarovv@gmail.com>
2024-09-20 18:25:49 +0600

    updating admin panel

--- loans/admin.py
+++ loans/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from loans.models import Loan
+
+admin.site.register(Loan)
--- readers/admin.py
+++ readers/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from readers.models import Reader
+
+admin.site.register(Reader)
